f85afa93d20ffbb35dd6a629748bd106285620cb
dylangetssmart <dylans@smartadvocate.com>
2025-01-29 14:42:20 -0500

    injuries

--- needles/sql/conversion/2_case/2.39_Injury_user_party.sql
+++ needles/sql/conversion/2_case/2.39_Injury_user_party.sql
@@ -1,54 +1,59 @@
-USE JoelBieberSA_Needles
-GO
+use JoelBieberSA_Needles
+go
+	
+alter table [sma_TRN_PlaintiffInjury] disable trigger all
+go
 
-/* ####################################
-1.0 -- Create PlaintiffInjury records
-*/
-ALTER TABLE [sma_TRN_PlaintiffInjury] DISABLE TRIGGER ALL
-GO
+-------------------------------------------------------------------------------
+-- Insert Injuries Summary
+-------------------------------------------------------------------------------
 
-INSERT INTO [sma_TRN_PlaintiffInjury]
+insert into [sma_TRN_PlaintiffInjury]
 	(
-	[plinPlaintiffID], [plinCaseID], [plisInjuriesSummary], [plisPleadingsSummary], [plisConfinementHospital], [plisConfinementBed], [plisConfinementHome], [plisConfinementIncapacitated], [plisComment], [plinRecUserID], [plidDtCreated], [plinModifyUserID], [plidDtModified]
+	[plinPlaintiffID],
+	[plinCaseID],
+	[plisInjuriesSummary],
+	[plisPleadingsSummary],
+	[plisConfinementHospital],
+	[plisConfinementBed],
+	[plisConfinementHome],
+	[plisConfinementIncapacitated],
+	[plisComment],
+	[plinRecUserID],
+	[plidDtCreated],
+	[plinModifyUserID],
+	[plidDtModified]
 	)
-	SELECT
-		pln.plnnPlaintiffID AS [plinPlaintiffID]
-	   ,cas.casnCaseID		AS [plinCaseID]
-	   ,ISNULL('Injuries: ' + NULLIF(CONVERT(VARCHAR(MAX), ucd.Injuries), '') + CHAR(13), '') +
-		ISNULL('Passenger''s Injuries: ' + NULLIF(CONVERT(VARCHAR(MAX), ucd.Passengers_Injuries), '') + CHAR(13), '') +
-		''					AS [plisInjuriesSummary]
-	   ,NULL				AS [plisPleadingsSummary]
-	   ,NULL				AS [plisConfinementHospital]
-	   ,NULL				AS [plisConfinementBed]
-	   ,NULL				AS [plisConfinementHome]
-	   ,NULL				AS [plisConfinementIncapacitated]
-	   ,NULL				AS [plisComment]
-	   ,368					AS [plinRecUserID]
-	   ,GETDATE()			AS [plidDtCreated]
-	   ,NULL				AS [plinModifyUserID]
-	   ,NULL				AS [plidDtModified]
-	FROM NeedlesSLF..user_case_data ucd
-	JOIN sma_trn_Cases cas
-		ON cas.cassCaseNumber = CONVERT(VARCHAR, ucd.casenum)
-	-- Link to SA Contact Card via:
-	-- user_tab_data -> user_tab_name -> names -> IndvOrgContacts_Indexed
-	--join NeedlesSLF.dbo.user_case_name ucn
-	--	on ucd.casenum = ucn.casenum
-	--join NeedlesSLF.dbo.names n
-	--	on ucn.user_name = n.names_id
-	--left join IndvOrgContacts_Indexed ioci
-	--	on n.names_id = ioci.saga
-	--	and ioci.CTG = 1
-	JOIN [sma_TRN_Plaintiff] pln
-		ON pln.plnnCaseID = cas.casnCaseID
-	WHERE ISNULL(ucd.Injuries, '') <> ''
-		OR ISNULL(ucd.Passengers_Injuries, '') <> ''
-GO
+	select
+		pln.plnnPlaintiffID as [plinplaintiffid],
+		cas.casnCaseID		as [plincaseid],
+		ISNULL('Injuries: ' + NULLIF(CONVERT(VARCHAR(MAX), upd.Injuries), '') + CHAR(13), '') +
+		ISNULL('Current Injuries: ' + NULLIF(CONVERT(VARCHAR(MAX), upd.current_Injuries), '') + CHAR(13), '') +
+		ISNULL('Current Inj: ' + NULLIF(CONVERT(VARCHAR(MAX), upd.CURRENT_INJ), '') + CHAR(13), '') +
+		''					as [plisinjuriessummary],
+		null				as [plispleadingssummary],
+		null				as [plisconfinementhospital],
+		null				as [plisconfinementbed],
+		null				as [plisconfinementhome],
+		null				as [plisconfinementincapacitated],
+		null				as [pliscomment],
+		368					as [plinrecuserid],
+		GETDATE()			as [pliddtcreated],
+		null				as [plinmodifyuserid],
+		null				as [pliddtmodified]
+	--select *
+	from JoelBieberNeedles..user_party_data upd
+	join sma_trn_Cases cas
+		on cas.cassCaseNumber = CONVERT(VARCHAR, upd.case_id)
+	join [sma_TRN_Plaintiff] pln
+		on pln.plnnCaseID = cas.casnCaseID
+	where ISNULL(upd.Injuries, '') <> ''
+		or ISNULL(upd.Current_Injuries, '') <> ''
+		or ISNULL(upd.CURRENT_INJ, '') <> ''
+go
 
-ALTER TABLE [sma_TRN_PlaintiffInjury] ENABLE TRIGGER ALL
-GO
--- SET IDENTITY_INSERT [sma_TRN_PlaintiffInjury] OFF;
-GO
+alter table [sma_TRN_PlaintiffInjury] enable trigger all
+go
 
 
 /* ####################################
