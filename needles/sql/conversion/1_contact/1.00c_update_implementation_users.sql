6beaa601078cb8c27d1a9bb5a9f8a046227c716f
dylangetssmart <dylans@smartadvocate.com>
2025-02-06 16:10:41 -0500

    dev

--- needles/sql/conversion/1_contact/1.00c_update_implementation_users.sql
+++ needles/sql/conversion/1_contact/1.00c_update_implementation_users.sql
@@ -74,16 +74,26 @@ go
 --end
 --go
 
-update sma_mst_users
-set source_id = (
-		select top 1
-			s.staff_code
-		from JoelBieberNeedles..staff s
-		join [JoelBieberSA_Needles]..sma_MST_IndvContacts indv
-			on s.full_name = indv.cinsFirstName + ' ' + indv.cinsLastName
-		where indv.cinnContactID = sma_MST_Users.usrnContactID
-	),
-	source_db = 'needles';
+-- Attempt to match via name
+--update sma_mst_users
+--set source_id = (
+--		select top 1
+--			s.staff_code
+--		from JoelBieberNeedles..staff s
+--		join [JoelBieberSA_Needles]..sma_MST_IndvContacts indv
+--			on s.full_name = indv.cinsFirstName + ' ' + indv.cinsLastName
+--		where indv.cinnContactID = sma_MST_Users.usrnContactID
+--	),
+--	source_db = 'needles';
+
+-- Use mapping spreadsheet
+update sma_MST_Users
+set source_id = imp.staffcode,
+	source_db = 'needles'
+from sma_mst_users u
+join Implementation_Users_1738864619 imp
+	on u.usrnUserID = imp.SAUserID
+where u.source_id is null
 
 --UPDATE sma_MST_IndvContacts
 --set source_id = (
@@ -96,30 +106,30 @@ set source_id = (
 
 
 -- Step 1: Create a temporary table for mapping contact IDs to source IDs
-IF OBJECT_ID('tempdb..#ContactSourceMap') IS NOT NULL
-    DROP TABLE #ContactSourceMap;
+if OBJECT_ID('tempdb..#ContactSourceMap') is not null
+	drop table #ContactSourceMap;
 
-SELECT 
-    indv.cinnContactID AS ContactID,
-    u.source_id AS SourceID
-INTO #ContactSourceMap
-FROM sma_MST_Users u
-JOIN sma_MST_IndvContacts indv
-    ON u.usrnContactID = indv.cinnContactID;
+select
+	indv.cinnContactID as ContactID,
+	u.source_id		   as SourceID
+into #ContactSourceMap
+from sma_MST_Users u
+join sma_MST_IndvContacts indv
+	on u.usrnContactID = indv.cinnContactID;
 
 -- Step 2: Create an index on the temporary table for faster joins
-CREATE INDEX idx_ContactID ON #ContactSourceMap(ContactID);
+create index idx_ContactID on #ContactSourceMap (ContactID);
 
 -- Step 3: Update sma_MST_IndvContacts using the temporary table
-UPDATE indv
-SET indv.source_id = map.SourceID,
-    indv.source_db = 'needles'
-FROM sma_MST_IndvContacts indv
-JOIN #ContactSourceMap map
-    ON indv.cinnContactID = map.ContactID;
+update indv
+set indv.source_id = map.SourceID,
+	indv.source_db = 'needles'
+from sma_MST_IndvContacts indv
+join #ContactSourceMap map
+	on indv.cinnContactID = map.ContactID;
 
 -- Step 4: Clean up the temporary table
-DROP TABLE #ContactSourceMap;
+drop table #ContactSourceMap;
 
 --SELECT smu.usrsLoginID, smu.source_id FROM JoelBieberSA_Needles..sma_MST_Users smu order by smu.usrsLoginID
 
@@ -129,3 +139,4 @@ DROP TABLE #ContactSourceMap;
 --			ON smic.cinnContactID = u.usrnContactID
 --		LEFT JOIN JoelBieberNeedles..staff s
 --			ON s.full_name = smic.cinsFirstName + ' ' + smic.cinsLastName
+
